8b6f3980f8b582fa7d63a900d9a26bb9830ed47f
Administrator <Administrator@Rajesh>
2020-06-27 15:22:07 +0530

    My First Project to GitHUB

---
+++ RAS/src/test/java/com/ras/testscript/TestFrameWork.java
@@ -0,0 +1,14 @@
+package com.ras.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestFrameWork {
+	
+	@Test
+	public void tc_01()
+	{
+		Reporter.log("First Test Case",true);
+	}
+
+}
