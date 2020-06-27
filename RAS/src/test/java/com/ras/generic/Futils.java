c8bdf93356efc4c5f7274549e6cd5b964585361d
Administrator <Administrator@Rajesh>
2020-06-27 15:39:53 +0530

    Made some changes

--- RAS/src/test/java/com/ras/generic/Futils.java
+++ RAS/src/test/java/com/ras/generic/Futils.java
@@ -1,5 +1,20 @@
 package com.ras.generic;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Futils {
+	
+	@Test
+	public void tc_01()
+	{
+		Reporter.log("First Test Case",true);
+	}
+	
+	@Test
+	public void tc_02()
+	{
+		Reporter.log("Second Test Case",true);
+	}
 
 }
